940e9b6ba77a6bb093eb0709fa48add476c9bdc1
Colin Copeland <copelco@caktusgroup.com>
2013-04-15 18:34:26 -0400

    not sure what ADMIN_MEDIA_ROOT did.. we use staticfiles now

--- OpenDataCatalog/urls.py
+++ OpenDataCatalog/urls.py
@@ -80,8 +80,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^_admin_/', include(admin.site.urls)),
 
-    (r'^/static/admin_media/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': settings.ADMIN_MEDIA_ROOT}), 
     (r'^static/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': settings.STATIC_DATA}),
     (r'^media/(?P<path>.*)$', 'django.views.static.serve',
